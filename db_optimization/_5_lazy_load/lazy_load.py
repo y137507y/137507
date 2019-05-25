e6440407c3ae55be00e309d81b4ca59d08feeba5
y137507y <y13794739507@163.com>
2019-05-25 14:44:24 +0800

    first commit

---
+++ db_optimization/_5_lazy_load/lazy_load.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python                                                                  
+# -*-encoding=utf8 -*-                                                             
+# @Author         : imooc
+# @Email          : imooc@foxmail.com
+# @Created at     : 2018/12/26
+# @Filename       : lazy_load.py
+# @Desc           :
+
+
+
+
+import os
+import time
+import django
+import random
+import hashlib
+
+from backend import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from authorization.models import User
+from apis.models import App
+
+def lazy_load():
+    for user in User.objects.all()[:100]:
+        print(user.menu.all())
+
+def pre_load():
+    for user in User.objects.all()[:100].prefetch_related('menu'):
+        print(user.menu.all())
+
+
+if __name__ == '__main__':
+    pre_load()
---
+++ utils/proxy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python                                                                  
+# -*-encoding=utf8 -*-                                                             
+# @Author         : imooc
+# @Email          : imooc@foxmail.com
+# @Created at     : 2018/11/2
+# @Filename       : proxy.py
+# @Desc           :
+
+import backend.settings
+
+
+def proxy():
+    if backend.settings.USE_PROXY:
+        return {}
+    else:
+        return {}
